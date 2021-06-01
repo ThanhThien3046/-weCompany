b309a1dfc457fbd50798f8578ab7b43f24182fd5
thanh thien <thanhthien3046@gmail.com>
2021-06-01 09:20:55 +0900

    Thien

--- SERVER_PHP/resources/views/svg/ranking.blade.php
+++ SERVER_PHP/resources/views/svg/ranking.blade.php
@@ -2,23 +2,32 @@
 <!-- Generator: Adobe Illustrator 25.2.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="レイヤー_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
 	 y="0px" viewBox="0 0 134.91 134" style="enable-background:new 0 0 134.91 134;" xml:space="preserve">
-<circle class="cls-1" cx="85.34" cy="79.62" r="15.16"/>
-<circle class="cls-3" style="fill:none;stroke:#55A8CD;stroke-width:7;stroke-miterlimit:10;" cx="67.33" cy="67" r="62.59"/>
-<ellipse class="cls-3"  cx="51.98" cy="44.69" rx="2.66" ry="4.26"/>
-<ellipse class="cls-3"  cx="81.77" cy="44.69" rx="2.66" ry="4.26"/>
-<ellipse class="cls-3"  cx="67.33" cy="44.69" rx="2.66" ry="4.26"/>
-<path class="cls-3" d="M40.07,45.25V89.6h32.46l-2.54-5.61l-25.24-0.13l0.13-29.52H90.3v9.48l3.87,1.34
+
+<style type="text/css">
+	.st0{fill:#55A8CD;}
+	.sidebar__nav-link:hover .circle{ stroke: red;} 
+	.sidebar__nav-link:hover .st0{ fill: red; } 
+	.st1{fill:none;stroke:#55A8CD;stroke-width:7;stroke-miterlimit:10;}
+	.st2{fill:none;stroke:#55A8CD;stroke-width:2;stroke-miterlimit:10;}
+	.st3{fill:none;stroke:#F9F9F9;stroke-width:2;stroke-miterlimit:10;}
+</style>
+<circle class="st0" cx="85.34" cy="79.62" r="15.16"/>
+<circle class="st1 circle"  cx="67.33" cy="67" r="62.59"/>
+<ellipse class="st0"  cx="51.98" cy="44.69" rx="2.66" ry="4.26"/>
+<ellipse class="st0"  cx="81.77" cy="44.69" rx="2.66" ry="4.26"/>
+<ellipse class="st0"  cx="67.33" cy="44.69" rx="2.66" ry="4.26"/>
+<path class="st0" d="M40.07,45.25V89.6h32.46l-2.54-5.61l-25.24-0.13l0.13-29.52H90.3v9.48l3.87,1.34
 	c0.01-6.63,0.03-13.27,0.04-19.9c-2.78,0-5.55,0-8.33,0c0.19,2.5-1.85,4.57-4.14,4.5c-2.2-0.07-4.07-2.1-3.87-4.5h-6.81
 	c0.26,2.53-1.6,4.63-3.65,4.76c-2.22,0.15-4.48-2.01-4.23-4.76h-7.35c0.16,2.37-1.65,4.36-3.78,4.47c-2.28,0.12-4.39-1.93-4.23-4.47
 	H40.07z"/>
-<rect x="49.32" y="57.94" class="cls-3" width="8.12" height="5.08"/>
-<rect x="63.06" y="57.94" class="cls-3" width="8.12" height="5.08"/>
-<rect x="63.06" y="67" class="cls-3" width="8.12" height="5.08"/>
-<rect x="63.06" y="75.55" class="cls-3" width="6.28" height="5.08"/>
-<rect x="76.95" y="57.94" class="cls-3" width="8.12" height="5.08"/>
-<rect x="49.32" y="67" class="cls-3" width="8.12" height="5.08"/>
-<rect x="49.32" y="75.55" class="cls-3" width="8.12" height="5.08"/>
-<polyline style="fill:none;stroke:#55A8CD;stroke-width:2;stroke-miterlimit:10;" points="85.34,72.08 85.34,79.62 91.9,85.33 "/>
-<polyline style="fill:none;stroke:#F9F9F9;stroke-width:2;stroke-miterlimit:10;" points="85.34,72.08 85.34,79.62 91.9,85.33 "/>
+<rect x="49.32" y="57.94" class="st0" width="8.12" height="5.08"/>
+<rect x="63.06" y="57.94" class="st0" width="8.12" height="5.08"/>
+<rect x="63.06" y="67" class="st0" width="8.12" height="5.08"/>
+<rect x="63.06" y="75.55" class="st0" width="6.28" height="5.08"/>
+<rect x="76.95" y="57.94" class="st0" width="8.12" height="5.08"/>
+<rect x="49.32" y="67" class="st0" width="8.12" height="5.08"/>
+<rect x="49.32" y="75.55" class="st0" width="8.12" height="5.08"/>
+<polyline class="st2" style="fill:none;stroke:#55A8CD;stroke-width:2;stroke-miterlimit:10;" points="85.34,72.08 85.34,79.62 91.9,85.33 "/>
+<polyline class="st3" style="fill:none;stroke:#F9F9F9;stroke-width:2;stroke-miterlimit:10;" points="85.34,72.08 85.34,79.62 91.9,85.33 "/>
 </svg>
 
